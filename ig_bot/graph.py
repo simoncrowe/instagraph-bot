f5b0d9ae3e7682d1b13525150b0834f8e204f955
Simon Crowe <info@simoncrowe.net>
2020-08-16 20:24:18 +0100

    Simplified scraping functions, added centrality.

--- ig_bot/graph.py
+++ ig_bot/graph.py
@@ -1,41 +1,64 @@
 """Contains social graph related logic."""
 
+from collections import defaultdict
 import logging
-from typing import Generator, Tuple
+from operator import itemgetter
+from typing import Generator, Iterable, Tuple
 
 import networkx as nx
 
 from ig_bot.data import AccountSummary, account_summary_to_camel_case
 
 
+IN_DEGREE_CENTRALITY = 'IN_DEGREE_CENTRALITY'
+EIGENVECTOR_CENTRALITY = 'EIGENVECTOR_CENTRALITY'
+
 CENTRALITY_METRIC_FUNCTIONS = {
-    'IN_DEGREE_CENTRALITY': nx.in_degree_centrality,
-    'EIGENVECTOR_CENTRALITY': nx.eigenvector_centrality,
+    IN_DEGREE_CENTRALITY: nx.in_degree_centrality,
+    EIGENVECTOR_CENTRALITY: nx.eigenvector_centrality,
 }
 
 
-def add_nodes(graph: nx.DiGraph, *nodes: Tuple[AccountSummary], logger: logging.Logger):
+def add_nodes(graph: nx.DiGraph, *nodes: Tuple[AccountSummary]):
     """Adds nodes to graph for AccountSummary instance is not already present. """
-    for node in nodes:
-        if node.identifier not in graph:
-            graph.add_node(node.identifier, 
-                           **account_summary_to_camel_case(node))
-            logger.info(f'Added  "{node}" to graph.')
-        else:
-            logger.info(f'Node "{node}" already in graph.')
+    graph.add_nodes_from(
+        (node.identifier, account_summary_to_camel_case(node))
+        for node in nodes
+    )
 
 
 def add_edges(graph: nx.DiGraph, 
-              source: AccountSummary, 
-              *destinations: Tuple[AccountSummary], 
-              logger: logging.Logger):
-    for destination in destinations:
-        graph.add_edge(u_of_edge=source.identifier, v_of_edge=destination.identifier)
-        logger.info(f'Created edge from {source} to {followed}.')
+              source: AccountSummary,
+              destinations: Iterable[AccountSummary]):
+    graph.add_edges_from(
+        (source.identifier, destination.identifier)
+        for destination in destinations
+    )
+
+def _combine(**attributes) -> defaultdict:
+    """Combines node attribute dicts into dicts of dicts.
+    e.g.
+    >> _combine(k1={0: 'a', 1: 'b'}, k2={0: 'c', 1: 'd'})
+    {0: {'k1': 'a', 'k2': 'c', 1}, 1: {'k1': 'b', 'k2': 'd'}}
+    """
+    combined = defaultdict(dict)
+
+    for name, pairs in dicts.items():
+        for k, v in pairs.items():
+            combined[k][name] = v
+
+    return combined
 
 
-def account_nodes_from_graph(
-        graph: nx.DiGraph, logger: logging.Logger
+def nodes_with_centrality(
+    graph: nx.DiGraph, centrality_algorithm: str
 ) -> Generator[AccountSummary, None, None]:
-    pass
+    centrality_function = CENTRALITY_METRIC_FUNCTIONS[centrality_algorithm]
+
+    data_by_id = _combine(centrality=centrality_function(graph),
+                          username=nx.get_node_attributes(graph, 'username'),
+                          full_name=nx.get_node_attributes(graph, 'fullName'))
+
+    for identifier, data in data_by_id.items():
+        yield AccountSummary(identifier=identifier, **data)
 
---
+++ ig_bot/tests/test_graph.py
@@ -0,0 +1,118 @@
+from unittest import mock
+
+import networkx as nx
+import pytest
+
+from ig_bot.factories import AccountSummaryFactory
+from ig_bot.graph import (
+    add_edges, 
+    add_nodes, 
+    IN_DEGREE_CENTRALITY,
+    nodes_with_centrality
+)
+
+
+@pytest.fixture
+def account_one_mock():
+    mock_ig_account = mock.Mock()
+    mock_ig_account.identifier = '1'
+    mock_ig_account.username = 'one'
+    mock_ig_account.full_name = 'Account One'
+
+    return mock_ig_account
+
+
+@pytest.fixture
+def account_one():
+    return AccountSummaryFactory(identifier='1',
+                                 username='one', 
+                                 full_name='Account One')
+
+
+@pytest.fixture
+def account_two():
+    return AccountSummaryFactory(identifier='2',
+                                 username='two',
+                                 full_name='Account Two')
+
+
+@pytest.fixture
+def account_three():
+    return AccountSummaryFactory(identifier='3',
+                                 username='three',
+                                 full_name='Account Three')
+
+
+def test_add_nodes_single(account_one):
+    graph = nx.DiGraph()
+
+    add_nodes(graph, account_one)
+    
+    assert list(nx.nodes(graph)) == [account_one.identifier]
+    
+    identifiers = nx.get_node_attributes(graph, 'identifier')
+    assert identifiers[account_one.identifier] == account_one.identifier 
+    usernames = nx.get_node_attributes(graph, 'username')
+    assert usernames[account_one.identifier] == account_one.username
+    full_names = nx.get_node_attributes(graph, 'fullName')
+    assert full_names[account_one.identifier] == account_one.full_name 
+    
+
+
+def test_add_nodes_multiple(account_one, account_two):
+    graph = nx.DiGraph()
+    
+    add_nodes(graph, account_one, account_two)
+    
+    assert list(nx.nodes(graph)) == [account_one.identifier,
+                                     account_two.identifier]
+    
+    identifiers = nx.get_node_attributes(graph, 'identifier')
+    assert identifiers[account_one.identifier] == account_one.identifier 
+    assert identifiers[account_two.identifier] == account_two.identifier 
+    usernames = nx.get_node_attributes(graph, 'username')
+    assert usernames[account_one.identifier] == account_one.username 
+    assert usernames[account_two.identifier] == account_two.username 
+    full_names = nx.get_node_attributes(graph, 'fullName')
+    assert full_names[account_one.identifier] == account_one.full_name 
+    assert full_names[account_two.identifier] == account_two.full_name 
+
+
+def test_add_edges(account_one, account_two, account_three):
+    graph = nx.DiGraph()
+    expected_edge_to_two = (account_one.identifier, account_two.identifier)
+    expected_edge_to_three = (account_one.identifier, account_three.identifier)
+
+    add_edges(graph, account_one, [account_two, account_three])
+    
+    assert graph.number_of_edges() == 2
+    edges = graph.edges([account_one.identifier])
+    assert expected_edge_to_two in edges
+    assert expected_edge_to_three in edges
+
+
+def test_nodes_with_centrality(account_one, account_two, account_three):
+    graph = nx.DiGraph()
+    add_nodes(graph, account_one, account_two, account_three)
+    add_edges(graph, account_one, [account_two, account_three])
+    add_edges(graph, account_two, [account_three])
+    
+    result_one, result_two, result_three = nodes_with_centrality(
+        graph, IN_DEGREE_CENTRALITY
+    )
+
+    assert result_one.identifier == account_one.identifier
+    assert result_one.username == account_one.username
+    assert result_one.full_name == account_one.full_name
+    assert result_one.centrality == 0    
+    
+    assert result_two.identifier == account_two.identifier
+    assert result_two.username == account_two.username
+    assert result_two.full_name == account_two.full_name
+    assert result_two.centrality == 0.5
+
+    assert result_three.identifier == account_three.identifier
+    assert result_three.username == account_three.username
+    assert result_three.full_name == account_three.full_name
+    assert result_three.centrality == 1
+

f6fe3c717088cd278bb5dc1334b489a39eb3aa8a
Simon Crowe <info@simoncrowe.net>
2020-08-30 18:34:18 +0100

    Refactor scrape following graph command setup.

--- ig_bot/scripts/scrape_following_graph.py
+++ ig_bot/scripts/scrape_following_graph.py
@@ -1,9 +1,12 @@
+import logging
 from os import path
+from pathlib import Path
+
 
 import click
 import yaml
 
-from ig_bot.graph import EIGENVECTOR_CENTRALITY
+from ig_bot.graph import CENTRALITY_METRIC_FUNCTIONS, EIGENVECTOR_CENTRALITY
 from ig_bot.scripts.util import (
     get_graph_file_path,
     initialise_logger,
@@ -20,41 +23,59 @@ from ig_bot.scripts.util import (
 @click.option(
     '--centrality-metric',
     '-c',
-    type=str,
+    type=click.Choice(CENTRALITY_METRIC_FUNCTIONS.keys(), case_sensitive=False),
     help='The measure determining the importance of an account.'
 )
 def scrape_following_graph(data_dir, username, log_level, centrality_metric):
     scrape_graph(data_dir, username, log_level, centrality_metric)
 
 
-def scrape_graph(data_dir: str,
-                 username: str = None,
-                 log_level: str = 'INFO',
-                 centrality_metric: str = EIGENVECTOR_CENTRALITY):
-
-    #with open('config.yaml') as file_obj:
-    #    config = yaml.safe_load(file_obj)
-
-    logger = initialise_logger(data_dir,
-                               'log',
-                               'ig_bot.scripts.scrape_following_graph',
-                               log_level)
-
+def _load_graph(data_dir: str, logger: logging.Logger):
     graph_path = path.join(data_dir, 'graph.gml')
     try:
-        graph = load_graph_gml(graph_path, logger)
+        return load_graph_gml(graph_path, logger)
     except OSError:
-        graph = None
+        return None
 
+
+def _load_accounts(data_dir: str, logger: logging.Logger):
     accounts_path = path.join(data_dir, 'accounts.csv')
     try:
-        accounts = load_dataframe_csv(accounts_path, logger)
+        return load_dataframe_csv(accounts_path, logger)
     except OSError:
-        accounts = None
+        return None
 
-    data_present = graph and accounts
 
-    if bool(data_present) == bool(username):
+def _get_logger(data_dir, log_level: str) -> logging.Logger:
+    return initialise_logger(data_dir,
+                             'log',
+                             'ig_bot.scripts.scrape_following_graph',
+                             log_level)
+
+
+def _load_config(config_path):
+    with open(config_path, 'r') as fileobj:
+        return taml.safe_load(fileobj)
+
+
+def scrape_graph(data_dir: str,
+                 username: str = None,
+                 centrality_metric: str = EIGENVECTOR_CENTRALITY,
+                 config_path: str = 'config.yaml',
+                 log_level: str = 'INFO'):
+
+    # Create data directory if absent
+    Path(data_dir).mkdir(parents=True, exist_ok=True)
+
+    config = _load_config(config_path)
+    logger = _get_logger(data_dir, log_level)
+
+    graph = _load_graph(data_dir, logger)
+    accounts = _load_accounts(data_dir, logger)
+
+    data_present = bool(graph) and bool(accounts)
+
+    if data_present == bool(username):
         raise ValueError(
             'Either a data directory with existing data must be '
             'provided or a username, not both.'
--- ig_bot/tests/scripts/test_scrape_following_graph.py
+++ ig_bot/tests/scripts/test_scrape_following_graph.py
@@ -7,24 +7,37 @@ import pytest
 from ig_bot.scripts.scrape_following_graph import scrape_graph
 
 
-def test_scrape_graph_no_username_and_empty_data_dir():
+
+@patch('ig_bot.scripts.scrape_following_graph._load_config', return_value={})
+def test_scrape_graph_no_username_and_nonexistent_data_dir(_):
     with tempfile.TemporaryDirectory() as temp_dir:
-        data_path = path.join(temp_dir, 'data')
-        mkdir(data_path)
- 
+        data_path = path.join(temp_dir, 'this_dir_does_not_exist')
+
         with pytest.raises(ValueError):
             scrape_graph(data_path)
 
-@patch("ig_bot.scripts.scrape_following_graph.load_dataframe_csv")
-@patch("ig_bot.scripts.scrape_following_graph.load_graph_gml")
-def test_scrape_graph_username_and_files_in_data_diri(*_):
+
+@patch('ig_bot.scripts.scrape_following_graph._load_config', return_value={})
+def test_scrape_graph_no_username_and_empty_data_dir(_):
+    with tempfile.TemporaryDirectory() as temp_dir:
+        data_path = path.join(temp_dir, 'data')
+        mkdir(data_path)
+
+        with pytest.raises(ValueError):
+            scrape_graph(data_path)
+
+
+@patch('ig_bot.scripts.scrape_following_graph._load_config', return_value={})
+@patch('ig_bot.scripts.scrape_following_graph.load_dataframe_csv')
+@patch('ig_bot.scripts.scrape_following_graph.load_graph_gml')
+def test_scrape_graph_username_and_files_in_data_dir(*_):
     with tempfile.TemporaryDirectory() as temp_dir:
         data_path = path.join(temp_dir, 'data')
         mkdir(data_path)
 
         graph_path = path.join(data_path, 'graph.gml')
         open(graph_path, 'w').close()
-        
+
         accounts_path = path.join(data_path, 'accounts.csv')
         open(accounts_path, 'w').close()
 
